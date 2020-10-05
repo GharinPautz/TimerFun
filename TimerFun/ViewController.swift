fa17d728b5a8b91a4af5fcfa830f91c7315ec95d
Gharin Pautz <gharinpautz@gmail.com>
2020-10-05 11:29:28 -0700

    Initial Commit

---
+++ TimerFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerFun
+//
+//  Created by Gharin Pautz on 10/5/20.
+//  Copyright © 2020 Gharin Pautz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
